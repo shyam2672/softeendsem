d2c57b00c8a98cf8d42038f6612788589a3b3059
shyam <shyamprajapati2672@gmail.com>
2023-04-20 22:02:43 +0530

    fff

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,17 +1,16 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
-const friendrequests=require("../models/friendrequests")
+const friendrequests = require("../models/friendrequests");
 module.exports.login = async (req, res, next) => {
   try {
-
     console.log(req.body);
     const { username, password } = req.body;
     const user = await User.findOne({ username });
     console.log(user);
     if (!user)
       return res.json({ msg: "Incorrect Username or Password", status: false });
-      console.log(user.password)
-     isPasswordValid = await bcrypt.compare(password, user.password);
+    console.log(user.password);
+    isPasswordValid = await bcrypt.compare(password, user.password);
     //  isPasswordValid = await(password == user.password)
     if (!isPasswordValid)
       return res.json({ msg: "Incorrect Username or Password", status: false });
@@ -24,72 +23,76 @@ module.exports.login = async (req, res, next) => {
 
 module.exports.getuserinfo = async (req, res, next) => {
   // console.log(req.body);
-    try {
-      const userid=req.params.id;
-      const user = await User.findOne({ id }).select({
-        username,email,rating,avatarImage,gender,
-      });
-  console.log(user);
-      // delete user.password;
-      return res.json({ status: true, user });
-    } catch (ex) {
-      console.log(ex);
-      next(ex);
-    }
+  try {
+    const userid = req.params.id;
+    const user = await User.findOne({ id }).select({
+      username,
+      email,
+      rating,
+      avatarImage,
+      gender,
+    });
+    console.log(user);
+    // delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    console.log(ex);
+    next(ex);
+  }
 };
-
 
 module.exports.register = async (req, res, next) => {
   console.log(req.body);
-    try {
-      const { username, email, password,gender } = req.body;
-      const usernameCheck = await User.findOne({ username });
-      if (usernameCheck)
-        return res.json({ msg: "Username already used", status: false });
-      const emailCheck = await User.findOne({ email });
-      if (emailCheck)
-        return res.json({ msg: "Email already used", status: false });
-      const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const { username, email, password, gender } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck)
+      return res.json({ msg: "Username already used", status: false });
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck)
+      return res.json({ msg: "Email already used", status: false });
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-      const user = await User.create({
-        email,
-        username,
-        gender,
-        password: hashedPassword,
-      });
-  console.log(user);
+    const user = await User.create({
+      email,
+      username,
+      gender,
+      password: hashedPassword,
+    });
+    console.log(user);
 
-      delete user.password;
-      return res.json({ status: true, user });
-    } catch (ex) {
-      console.log(ex);
-      next(ex);
-    }
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    console.log(ex);
+    next(ex);
+  }
 };
 
-module.exports.sendrequest= async (req,res,next)=>{
+module.exports.sendrequest = async (req, res, next) => {
   try {
     // const user = await User.find({ _id:  req.params.id  });
-    const sender=req.params.senderid;
-    const receiver=req.params.receiverid;
-  const request=friendrequests.create({sender,receiver});
-   console.log(request);
-    return res.json({ status: true, request }); 
+    const sender = req.params.senderid;
+    const receiver = req.params.receiverid;
+    const request = friendrequests.create({ sender, receiver });
+    console.log(request);
+    return res.json({ status: true, request });
   } catch (ex) {
     next(ex);
-  } 
-}
+  }
+};
 
-module.exports.addfriend = async (req,res,next)=>{
+module.exports.addfriend = async (req, res, next) => {
   try {
     // const user = await User.find({ _id:  req.params.id  });
-    const sender=req.params.senderid;
-    const receiver=req.params.receiverid;
+    const sender = req.params.senderid;
+    const receiver = req.params.receiverid;
     // const user=user.findOne({receiver});
-   const user= User.findOneAndUpdate(
+    const user = User.findOneAndUpdate(
       { _id: receiverid },
       { $push: { friends: sender } },
-      { new: true }, (err, user) => {
+      { new: true },
+      (err, user) => {
         if (err) {
           // handle error
         } else {
@@ -97,39 +100,33 @@ module.exports.addfriend = async (req,res,next)=>{
           console.log(user);
           return res.json({ status: true, user });
         }
-      })
-  // const request=friendrequests.create({sender,receiver});
-   
+      }
+    );
+    // const request=friendrequests.create({sender,receiver});
   } catch (ex) {
     next(ex);
-  } 
-}
+  }
+};
 module.exports.getrequests = async (req, res, next) => {
   try {
     // const user = await User.find({ _id:  req.params.id  });
-   const requests= friendrequests.find({receiver:req.params.id})
+    const requests = friendrequests.find({ receiver: req.params.id });
 
-   console.log(requests);
-    return res.json(requests); 
+    console.log(requests);
+    return res.json(requests);
   } catch (ex) {
     next(ex);
   }
 };
 
-
 module.exports.getfriends = async (req, res, next) => {
   try {
     // const user = await User.find({ _id:  req.params.id  });
-   const friends= User.findById({_id: req.params.id})
-  .populate('friends').select(
-    "email",
-    "username",
-    "avatarImage",
-    "_id",
-    "rating",
-  )
-   console.log(friends);
-    // return res.json(users); 
+    const friends = User.findById({ _id: req.params.id })
+      .populate("friends")
+      .select("email", "username", "avatarImage", "_id", "rating");
+    console.log(friends);
+    // return res.json(users);
   } catch (ex) {
     next(ex);
   }
