91e4d4789ecfca906f11faab969f321a2a65fb4f
shyam <shyamprajapati2672@gmail.com>
2023-04-28 03:09:47 +0530

    socket done

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 // const userModel = require()
-const bodyParser=require("body-parser");
+const bodyParser = require("body-parser");
 const User = require("./models/userModel");
 const socket = require("socket.io");
+const utils = require("./utils.js");
+const uniqueID = require("uniqid");
+
+// require("./globals.js");
 
 // const authRoutes = require("./routes/auth");
 // const messageRoutes = require("./routes/messages");
@@ -12,9 +16,11 @@ const app = express();
 // const socket = require("socket.io");
 require("dotenv").config();
 const authRoutes = require("./routes/userRoutes");
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(cors());
 app.use(express.json());
 
@@ -44,27 +50,158 @@ const io = socket(server, {
 });
 
 global.onlineUsers = new Map();
+global.randomonlineUsers = [];
+global.availableUsers = [];
+global.rooms = [];
+global.queue = [];
+
 io.on("connection", (socket) => {
   global.chatSocket = socket;
+  console.log(onlineUsers);
+  // console.log(randomonlineUsers);
+  console.log(rooms);
+  console.log("fuck");
   socket.on("add-user", (userId) => {
     onlineUsers.set(userId, socket.id);
-    console.log(onlineUsers)
+    // console.log(onlineUsers);
   });
 
   socket.on("send-msg", (data) => {
-    console.log(onlineUsers)
     const sendUserSocket = onlineUsers.get(data.to);
-    console.log(sendUserSocket)
-    console.log(data.to)
+    // console.log(sendUserSocket);
+    // console.log(data.to);
     if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-recieve", data.msg,data.from,data.to);
+      socket
+        .to(sendUserSocket)
+        .emit("msg-recieve", data.msg, data.from, data.to);
+    } else {
+      console.log("fidptr");
     }
-    else{
-      console.log("fidptr")
+  });
+
+  let windowID = socket;
+  socket.emit("wait", {
+    message: "Please wait...connecting you to stranger!",
+  });
+  //push the user to avilable users list
+  availableUsers.push(socket);
+  let resolveAfter5Seconds = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve("resolved");
+      }, 5000);
+    });
+  };
+  async function asyncCall() {
+    let result = await resolveAfter5Seconds();
+    console.log("Fff");
+
+    //get index of randomly selected user from the available users list
+    let selected = Math.floor(Math.random() * availableUsers.length);
+    //store the user in Socket
+
+    socket = availableUsers[selected];
+
+    //remove the randomly selected user from the available users list
+    availableUsers.splice(selected, 1);
+
+    // Make a user object and add it to the onlineUsers list and rooms too(maybe we can add to room once we have the partner.)
+    // create an unique id here.
+    // let uID = uniqueID();
+    // console.log(uID);
+    // rooms.push({ "roomID": uID, "isFilled": false });
+    // // Maintain a global room array which would store the room ids.
+    // socket.join(uID);
+    // // emit the room id to the frontend side.
+    // socket.emit('private ack', { "message": "Added to privateRoom", "roomID": uID });
+    socket.emit("ack", { id: socket.id, msg: "User connected" });
+    randomonlineUsers.push(socket);
+    console.log("fuckyou");
+
+    socket.on("privateRoom", (user) => {
+      console.log("ffff");
+
+      let unfilledRooms = rooms.filter((room) => {
+        if (!room.isFilled) {
+          return room;
+        }
+      });
+
+      // console.log(unfilledRooms);
+      try {
+        // join the existing room.
+        socket.join(unfilledRooms[0].roomID);
+        let index = rooms.indexOf(unfilledRooms[0]);
+        rooms[index].isFilled = true;
+        unfilledRooms[0].isFilled = true;
+        console.log(rooms);
+        socket.emit("private ack", {
+          message: "Added to privateRoom",
+          roomID: unfilledRooms[0].roomID,
+          isfilled:false,
+        });
+        socket.roomID = unfilledRooms[0].roomID;
+        io.sockets
+          .in(socket.roomID)
+          .emit("toast", { message: "You are connected with a stranger!" });
+      } catch (e) {
+        // dont have unfilled rooms. Thus creating a new user.
+        let uID = uniqueID();
+        rooms.push({ roomID: uID, isFilled: false });
+        socket.join(uID);
+        socket.roomID = uID;
+        console.log(rooms);
+
+        socket.emit("private ack", {
+          message: "Added to privateRoom",
+          roomID:uID,
+          isfilled: true,
+        });
+      }
+    });
+  }
+  console.log("sdfrwsd");
+  asyncCall();
+
+  socket.on("sendMessage", (data) => {
+    // let timeStamp = moment().format("LT");
+    console.log(data);
+    io.sockets.in(data.room).emit("newMessage", {
+      message: data.message,
+      senderId: data.from,
+      roomID:data.roomid,
+      // timeStamp: timeStamp,
+    });
+  });
+
+  socket.on("typing", (data) => {
+    io.sockets.in(data.room).emit("addTyping", {
+      senderId: windowID.id,
+      typingStatus: data.typingStatus,
+    });
+  });
+
+  // Disconnect the user
+  socket.on("disconnect", () => {
+    let index = randomonlineUsers.indexOf(socket);
+    randomonlineUsers.splice(index, 1);
+    index = rooms.findIndex((x) => x.roomID == windowID.roomID);
+    if (index >= 0) {
+      if (rooms[index].isFilled == true) {
+        let warning = {
+          title: "Stranger is disconnected!",
+          message: "Please click on 'New' button to connect to someone else.",
+        };
+        io.sockets
+          .in(windowID.roomID)
+          .emit("alone", { warning: warning, roomID: windowID.roomID });
+        rooms.splice(index, 1);
+      } else {
+        rooms.splice(index, 1);
+      }
     }
   });
 });
-
 // // user1 =  User.
 // run1();
 
