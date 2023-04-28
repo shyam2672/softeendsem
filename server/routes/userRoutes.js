49e45c3bca5861fe6da57fc11ea8cc911f356006
Prashant Mittal <75663788+mittal-prashant@users.noreply.github.com>
2023-04-28 14:31:30 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,11 +3,11 @@ const userctrl = require("../controllers/userController.js");
 
 router.post("/register", userctrl.register);
 router.post("/login", userctrl.login);
-router.post("/setavatar/:id", userctrl.setAvatar);
+router.post("/setavatar", userctrl.setAvatar);
 router.post("/friends", userctrl.getfriends);
 router.post("/sendrequest", userctrl.sendrequest);
-router.get("/getrequests/:id", userctrl.getrequests);
+router.post("/getrequests", userctrl.getrequests);
 router.post("/addfriend", userctrl.addfriend);
-router.get("/getuserinfo/:id", userctrl.getuserinfo);
+router.post("/getuserinfo", userctrl.getuserinfo);
 
 module.exports = router;
