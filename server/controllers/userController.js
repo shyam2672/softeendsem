d72893be1ee1c0c564bf4d0f03ec167e17960f8e
Raj <rg23072003@gmail.com>
2023-05-03 13:40:59 +0530

    deployment changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/userModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const friendrequests = require("../models/friendrequests");
 const jwt = require("jsonwebtoken");
 
